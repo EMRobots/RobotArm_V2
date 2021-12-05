b377c33c306241e8cbdf85fd44cd1b7265ac84e6
EMRobots <erik.meyer931@gmail.com>
2021-12-05 15:45:04 +0100

    init

---
+++ Slave/Slave.ino
@@ -0,0 +1,68 @@
+#include <Wire.h>
+#include <AccelStepper.h>
+volatile signed char x,xalt,y,yalt,SavePos,SavePosalt = 0;
+int Pos = 0;  
+ AccelStepper stepper(1,5,4);
+ AccelStepper stepper2(1,8,7);
+void setup()
+{
+  Wire.begin(4);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(115200);           // start serial for output
+  pinMode(9, OUTPUT);
+  pinMode(9, LOW);
+  pinMode(6,OUTPUT);
+  digitalWrite(6, HIGH);
+  stepper.setAcceleration(500); 
+  stepper.setMaxSpeed(10000); 
+  stepper2.setAcceleration(500); 
+  stepper2.setMaxSpeed(10000);
+ 
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(200);
+  stepper.moveTo(1000);
+
+}
+
+void GetData()
+{
+  pinMode(9, HIGH);
+  pinMode(9,LOW);
+}
+
+void loop()
+{ 
+  GetData(); 
+  //Serial.println(SavePos, DEC);
+  // Serial.println("\n");
+  //Serial.println(y, DEC);
+ 
+  if(x>10 || x<-10){
+    if(x!= xalt){
+  stepper.setSpeed(x*5);} 
+  stepper.runSpeed();
+  }
+  if(y>10 || y<-10){
+    if(y!= yalt){
+  stepper2.setSpeed(y*10);} 
+  stepper2.runSpeed();
+  }
+ if(SavePos == 1)
+  {
+    SavePos = 0; 
+    stepper.setCurrentPosition(0); 
+    Serial.println("Test");
+  }
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany)
+{
+  xalt = x;
+  yalt = y;
+ x = Wire.read();
+ y = Wire.read();
+ SavePos = Wire.read();
+ 
+}
---
+++ master/master.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <Ps3Controller.h>
+
+volatile signed char x = 0;
+bool flag = false; 
+void IRAM_ATTR isr() {
+    flag = true; 
+}
+
+void setup()
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+  pinMode(15, INPUT_PULLUP);
+  attachInterrupt(15,isr,FALLING); 
+  pinMode(2,OUTPUT); 
+  Serial.begin(9600); 
+  Ps3.begin("60:f4:94:cb:d9:b3");
+}//sdasda
+
+
+
+void loop()
+{
+ sendTicker(); //GIT TEST;  
+}
+
+void sendTicker()
+{
+  if (flag == true)
+  {
+    Wire.beginTransmission(4); // transmit to device #4
+    Wire.write(Ps3.data.analog.stick.lx);
+    Wire.write(Ps3.data.analog.stick.ly);
+    if (Ps3.event.button_down.cross )
+    {Wire.write(1);}else{Wire.write(0);}
+    Wire.endTransmission();    // stop transmitting
+     //Serial.println(,DEC);
+    flag = false; 
+  }
+}
